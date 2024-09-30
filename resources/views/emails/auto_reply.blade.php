ab1270b3f86ddffcc59b2ca81e1a4e927cf7457f
Akatious1 <sule.aktious@gmail.com>
2024-09-30 22:06:46 +0000

    auto-reply

--- resources/views/emails/auto_reply.blade.php
+++ resources/views/emails/auto_reply.blade.php
@@ -1,12 +1,12 @@
 @extends('website.layouts.main')
 @section('title')
-    Contact
+Contact
 @endsection
 @section('content')
-    <h1>Thank You, {{ $messageContent['full_name'] }}!</h1>
-    <p>We have received your message and will get back to you as soon as possible.</p>
-    <p>Here's a copy of your message:</p>
+<h1>Thank You, {{ $messageContent['full_name'] }}!</h1>
+<p>We have received your message and will get back to you as soon as possible.</p>
+<!-- <p>Here's a copy of your message:</p>
     <p><strong>Subject:</strong> {{ $messageContent['subject'] }}</p>
-    <p><strong>Message:</strong> {{ $messageContent['message'] }}</p>
-    <p>Best regards,<br>TGR Africa Team</p>
+    <p><strong>Message:</strong> {{ $messageContent['message'] }}</p> -->
+<p>Best regards,<br>TGR Africa Team</p>
 @endsection
