5973ea61ea3f3b9f72f60877d5eb5fd5f73fcbf3
Isaac Totimeh <87771826+GreatLogi@users.noreply.github.com>
2024-11-11 15:48:24 +0000

    Updated on thew cookies

--- resources/views/website/layouts/main.blade.php
+++ resources/views/website/layouts/main.blade.php
@@ -106,37 +106,6 @@
         <i class="fa fa-whatsapp my-float"></i>
     </a>
 
-
-    <script>
-        function setCookie(name, value, days) {
-            const d = new Date();
-            d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-            const expires = "expires=" + d.toUTCString();
-            document.cookie = name + "=" + value + ";" + expires + ";path=/";
-        }
-
-        function getCookie(name) {
-            const nameEQ = name + "=";
-            const ca = document.cookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        }
-        // Check if the user has already visited the site
-        window.onload = function() {
-            if (!getCookie("userPreference")) {
-                // Set cookie for new visitors
-                setCookie("userPreference", "visited", 30); // 30 days
-                alert("Welcome! We have set a cookie for you.");
-            } else {
-                console.log("Welcome back! Cookie found.");
-            }
-        };
-    </script>
-
     <!-- End of whatsapp Button -->
     <!-- Vendor -->
     <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script>
